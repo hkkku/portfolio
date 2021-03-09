288e52454c94c5a9a8c6cac0a990e8d2450c16cb
hkkku <hhk7045@gmail.com>
2021-03-09 18:29:33 +0900

    03/09 기본 html 세팅 & 사이드 네비게이션 클릭 기능 구현

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(function(){
+  $(".sideMenu").click(function(){
+    $(this).toggleClass("on");
+    if($(this).hasClass("on")){
+      $(this).find("i").attr("class", "fa fa-times");
+      $(".sideMenu").attr("onclick", "closeNav()")
+    } else {
+      $(this).find("i").attr("class", "fa fa-bars");
+      $(".sideMenu").attr("onclick", "openNav()")
+    }
+  })
+});

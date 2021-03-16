a4c2d2e6a66974b1b5d9726366495c8572ffdc1c
hkkku <hhk7045@gmail.com>
2021-03-16 14:06:43 +0900

    03/16 slick 오류 수정

--- js/custom.js
+++ js/custom.js
@@ -10,6 +10,49 @@ $(function(){
     }
   });
 
- 
- 
+  $(".btnBox button").click(function(){
+    let index = $(this).index();
+
+    $(".potFrame").hide();
+    $(".potFrame").eq(index).css('display', 'flex');
+    $('.potSlider').eq(index).slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      lazyLoad: 'ondemand',
+      centerMode: true,
+      centerPadding: '0',
+      infinite: true,
+      speed: 500,
+      arrows: true,
+      autoplay: true,
+      easing: 'ease',
+      responsive: [
+        
+        {
+          breakpoint: 900,
+          settings: {
+            arrows: true,
+            // centerMode: true,
+            // centerPadding: '40px',
+            fade: true,
+            speed: 700,
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            // centerMode: true,
+            // centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+  });
+  
+  $(".btnBox button").eq(0).trigger("click");
+
+  
 });

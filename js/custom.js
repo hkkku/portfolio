c7b88cf592d314ca04e826c0545f22353fe3c290
hkkku <hhk7045@gmail.com>
2021-03-17 17:27:13 +0900

    03/17 모바일 드롭다운 메뉴 적용 & 모바일 사이즈까지 반응형 처리 완료

--- js/custom.js
+++ js/custom.js
@@ -56,5 +56,17 @@ $(function(){
   
   $(".btnBox button").eq(0).trigger("click");
 
-  
+  $(".mobileMenu").click(function(){
+    $(this).toggleClass("on");
+    // on 과 off로 분기를 나눌 수 있다 -> if 구문 사용 가능
+    if($(this).hasClass("on")){
+      $(this).find("i").attr("class", "fa fa-times");
+      $(this).find("i").css("color", "#eae6df");
+      $(".mobileCate").slideDown("fast");
+    } else {
+      $(this).find("i").attr("class", "fa fa-bars");
+      $(this).find("i").css("color", "#424266");
+      $(".mobileCate").slideUp("fast");
+    }
+  });
 });
